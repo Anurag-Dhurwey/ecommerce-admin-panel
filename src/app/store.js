77b78e9eab98edd00d5797632ea175395aeb9bcb
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2024-01-05 18:35:00 +0530

    resolving

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
-// import productReducer from "../features/products/productsSlice"
-
+import productReducer from "../features/product/productSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     customers: customerReducer,
-    // products:productReducer
+    products: productReducer,
   },
 });
